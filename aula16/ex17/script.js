2ab65d2c7c86e2fc4db6a03d3e8bcc97d800f9f5
Lucas Caitaino Barreto <lucas.caitano.barreto@gmail.com>
2025-11-17 16:32:58 -0300

    finalizei

--- aula16/ex17/script.js
+++ aula16/ex17/script.js
@@ -27,6 +27,7 @@ function adicionar(){
         var item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
@@ -36,6 +37,30 @@ function adicionar(){
 
 function finalizar(){
     if (valores.length == 0){
-        window.alert('Adicione valores antes de finalizar')
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        var total = valores.length
+        var maior = valores[0]
+        var menor = valores[0]
+        var soma = 0
+        var media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior){
+                maior = valores[pos]
+            }
+            if (valores[pos] < menor){
+                menor = valores[pos]
+            }
+                
+        }
+        media = soma / total
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>A soma dos valores é ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores é ${media}. </p>`
     }
 }
